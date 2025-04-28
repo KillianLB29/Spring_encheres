a296edf20b0132e0ad9cf048bc3c0dcd022ba77e
Erwan LE MEUR <erwan.lemeur2025@campus-eni.fr>
2025-04-28 13:53:36 +0200

    Ajout du BO Categorie

--- src/main/java/fr/eni/tp/spring_encheres/bo/Categorie.java
+++ src/main/java/fr/eni/tp/spring_encheres/bo/Categorie.java
@@ -1,8 +1,43 @@
 package fr.eni.tp.spring_encheres.bo;
 
 public class Categorie {
-    private long id;
+    private Integer idCategorie;
     private String libelle;
 
+
+    public Categorie() {
+
+    }
+
+    public Categorie(Integer idCategorie, String libelle) {
+        this.idCategorie = idCategorie;
+        this.libelle = libelle;
+    }
+
+
+    public Integer getIdCategorie() {
+        return idCategorie;
+    }
+
+
+    public void setIdCategorie(Integer idCategorie) {
+        this.idCategorie = idCategorie;
+    }
+
+
+    public String getLibelle() {
+        return libelle;
+    }
+
+
+    public void setLibelle(String libelle) {
+        this.libelle = libelle;
+    }
+
+
+    @Override
+    public String toString() {
+        return "Categorie [idCategorie=" + idCategorie + ", libelle=" + libelle + "]";
+    }
 
 }
