95ea52ce16e99ca7ad45ab63a7067fa5cdced885
KillianLB29 <killianlebellec@gmail.com>
2025-04-28 14:21:36 +0200

    Modif utilisateur et retrait DAO

--- src/main/java/fr/eni/tp/spring_encheres/dal/RetraitDAO.java
+++ src/main/java/fr/eni/tp/spring_encheres/dal/RetraitDAO.java
@@ -3,7 +3,11 @@ package fr.eni.tp.spring_encheres.dal;
 import fr.eni.tp.spring_encheres.bo.Retrait;
 
 public interface RetraitDAO {
-    Retrait read(Integer idRetrait);
+    Retrait read(long id);
 
     void save(Retrait retrait);
+
+    void delete(long id);
+
+    void findAll();
 }
