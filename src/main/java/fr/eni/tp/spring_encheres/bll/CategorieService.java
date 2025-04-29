fbedc882ba18e2aee496f634b9c5debe2e894d60
ygrit <j.gritsiyenko@gmail.com>
2025-04-29 12:32:08 +0200

    travail sur CategorieService

--- src/main/java/fr/eni/tp/spring_encheres/bll/CategorieService.java
+++ src/main/java/fr/eni/tp/spring_encheres/bll/CategorieService.java
@@ -1,4 +1,15 @@
 package fr.eni.tp.spring_encheres.bll;
 
+import fr.eni.tp.spring_encheres.bo.ArticleVendu;
+import fr.eni.tp.spring_encheres.bo.Categorie;
+
+import java.util.List;
+
 public interface CategorieService {
+    List<Categorie> consulterCategories();
+    Categorie consulterCategorieParId(long id);
+    void createCategorie(Categorie categorie);
+    void supprimerCategorie(long id);
+
+
 }
