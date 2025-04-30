62b699fc556597bb08962cec1d29c3fca35c0ca9
KillianLB29 <killianlebellec@gmail.com>
2025-04-30 13:49:59 +0200

    ProfilController , création des pages de consultations du profil et de modification du profil(la modification est a terminé encore)

---
+++ src/main/java/fr/eni/tp/spring_encheres/ihm/ProfilController.java
@@ -0,0 +1,35 @@
+package fr.eni.tp.spring_encheres.ihm;
+
+import fr.eni.tp.spring_encheres.bll.UtilisateurService;
+import fr.eni.tp.spring_encheres.bo.Utilisateur;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.SessionAttributes;
+
+@Controller
+@SessionAttributes("utilisateurSession")
+public class ProfilController {
+
+    private UtilisateurService utilisateurService;
+
+    public ProfilController(UtilisateurService utilisateurService) {
+        this.utilisateurService = utilisateurService;
+    }
+
+    @GetMapping("/monProfil")
+    public String afficherProfil(@ModelAttribute("utilisateurSession") Utilisateur utilisateurSession, Model model) {
+        model.addAttribute("utilisateur", utilisateurSession);
+        return "profil/monProfil";
+    }
+
+    @GetMapping("/profil/{pseudo}")
+    public String profil(@PathVariable("pseudo") String pseudo, Model model) {
+        Utilisateur util = utilisateurService.findByUserName(pseudo);
+        System.out.println(util);
+        model.addAttribute("utilisateur", util);
+        return "profil/profil";
+    }
+}
