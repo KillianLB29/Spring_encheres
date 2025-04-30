80d8058ed0489b43d3d5cc40b42e7de552de280b
KillianLB29 <killianlebellec@gmail.com>
2025-04-30 10:37:26 +0200

    blabla

--- src/main/java/fr/eni/tp/spring_encheres/ihm/AccueilController.java
+++ src/main/java/fr/eni/tp/spring_encheres/ihm/AccueilController.java
@@ -1,9 +1,11 @@
 package fr.eni.tp.spring_encheres.ihm;
 
 import fr.eni.tp.spring_encheres.bll.ArticleVenduService;
+import fr.eni.tp.spring_encheres.bll.CategorieService;
 import fr.eni.tp.spring_encheres.bll.EnchereService;
 import fr.eni.tp.spring_encheres.bll.UtilisateurService;
 import fr.eni.tp.spring_encheres.bo.ArticleVendu;
+import fr.eni.tp.spring_encheres.bo.Categorie;
 import fr.eni.tp.spring_encheres.bo.Enchere;
 import fr.eni.tp.spring_encheres.bo.Utilisateur;
 import fr.eni.tp.spring_encheres.dal.ArticleVenduDAO;
@@ -24,22 +26,27 @@ public class AccueilController {
     private ArticleVenduService articleVenduService;
     private EnchereService enchereService;
     private UtilisateurService utilisateurService;
+    private CategorieService categorieService;
 
 
-    public AccueilController(ArticleVenduService articleVenduService, EnchereService enchereService, UtilisateurService utilisateurService) {
+    public AccueilController(ArticleVenduService articleVenduService, EnchereService enchereService, UtilisateurService utilisateurService, CategorieService categorieService) {
         this.articleVenduService = articleVenduService;
         this.enchereService = enchereService;
         this.utilisateurService = utilisateurService;
+        this.categorieService = categorieService;
     }
 
     @GetMapping({"/", "/accueil"})
     public String afficherAccueil( @ModelAttribute("utilisateurSession") Utilisateur utilisateurSession, Model model) {
         // Récupération de toutes les enchères depuis la base
         List<ArticleVendu> articles = articleVenduService.consulterArticlesEnCoursDeVente();
+        List<Categorie> categories = categorieService.consulterCategories();
+
 
         // Passage des données au modèle pour affichage dans la vue Thymeleaf
         model.addAttribute("articles", articles);
         model.addAttribute("utilisateur", utilisateurSession);
+        model.addAttribute("categories", categories);
 
         // Redirection vers le template index.html
         return "index";
