a1b3b202917d4827bd680f63166726fa0bf35823
KillianLB29 <killianlebellec@gmail.com>
2025-05-05 13:56:54 +0200

    Fixes des problèmes lorsque l'utilisateur enchérit

--- src/main/java/fr/eni/tp/spring_encheres/bll/EnchereServiceImpl.java
+++ src/main/java/fr/eni/tp/spring_encheres/bll/EnchereServiceImpl.java
@@ -39,21 +39,43 @@ public class EnchereServiceImpl implements EnchereService {
         {
             System.out.println("L'utilisateur n'a pas assez de crédit pour enchérir!");
         }
-        else{
-            List<Enchere> encheres = enchereDAO.read(enchere.getArticleVendu().getNoArticle());
-            encheres = encheres.stream()
-                    .filter(e -> e.getUtilisateur().getNoUtilisateur()==enchere.getUtilisateur().getNoUtilisateur())
-                    .collect(Collectors.toList());
-            if(encheres.size() > 0){
-                enchereDAO.delete(enchere.getArticleVendu().getNoArticle(),enchere.getUtilisateur().getNoUtilisateur());
-            }
-            enchereDAO.save(enchere);
-            Utilisateur utilisateur = utilisateurDAO.read(enchere.getUtilisateur().getNoUtilisateur());
-            utilisateur.setCredit(utilisateur.getCredit()-enchere.getMontantEnchere());
-            utilisateurDAO.update(utilisateur);
-        }
+        else
+        {
+            //Récupération de l'ancienne meilleur enchère pour récupérer l'utilisateur, lui rendre ses crédits car il n'est dorénavant plus la personne qui paye l'objet
+            //On la modifie aprés qu'on soit sur que l'ajout de la nouvelle est fonctionné
 
-    }
+            Enchere meilleurEnchere = meilleurEnchere(enchere.getArticleVendu().getNoArticle());
+            if(meilleurEnchere.getUtilisateur().getNoUtilisateur()==enchere.getUtilisateur().getNoUtilisateur()){
+                System.out.println("Vous etes déja la meilleur offre pour cet objet!");
+            }
+            else{
+                List<Enchere> encheres = enchereDAO.read(enchere.getArticleVendu().getNoArticle());
+                encheres = encheres.stream()
+                        .filter(e -> e.getUtilisateur().getNoUtilisateur()==enchere.getUtilisateur().getNoUtilisateur())
+                        .collect(Collectors.toList());
+                if(encheres.size() > 0){ //On a déja une enchère en cours on va donc l'update
+                    //Si on a déja une enchere en cours je la supprime et j'enregistre la nouvelle
+                    enchereDAO.delete(enchere.getArticleVendu().getNoArticle(),enchere.getUtilisateur().getNoUtilisateur());
+                    enchereDAO.save(enchere);
+
+                }
+                else{
+                    //Sinon Enregistrement de la nouvelle meilleur enchère et substraction des points de l'utilisateur
+                    enchereDAO.save(enchere);
+                }
+                //je soustrais en suite les points de l'utilisateur
+                Utilisateur utilisateur = utilisateurDAO.read(enchere.getUtilisateur().getNoUtilisateur());
+                utilisateur.setCredit(utilisateur.getCredit()-enchere.getMontantEnchere());
+                utilisateurDAO.update(utilisateur);
+                //On gère maintenant l'ancienne meilleur enchère
+                //Je rend les crédits a l'utilisateur
+                Utilisateur ancienGagnant = utilisateurDAO.read(meilleurEnchere.getUtilisateur().getNoUtilisateur());
+                ancienGagnant.setCredit(ancienGagnant.getCredit()+meilleurEnchere.getMontantEnchere());
+                utilisateurDAO.update(ancienGagnant);
+                }
+            }
+
+        }
 
     @Override
     public void supprimerEnchere(long idArticle,long idUtilisateur) {
@@ -70,6 +92,7 @@ public class EnchereServiceImpl implements EnchereService {
         List<Enchere> encheres = enchereDAO.read(idArticle);
         if(encheres.size() == 0)
         {
+            System.out.println("pas d'enchere");
             Utilisateur util = new Utilisateur();
             util.setPseudo("");
             Enchere enchere = new Enchere();
@@ -80,7 +103,10 @@ public class EnchereServiceImpl implements EnchereService {
             Enchere meilleureEnchere = encheres.stream()
                     .max(Comparator.comparing(Enchere::getMontantEnchere))
                     .orElse(null);
+            System.out.println("Enchere : "+meilleureEnchere);
             meilleureEnchere.setUtilisateur(utilisateurDAO.read(meilleureEnchere.getUtilisateur().getNoUtilisateur()));
+            meilleureEnchere.setArticleVendu(articleVenduDAO.read(idArticle));
+            System.out.println("meilleur enchere : "+meilleureEnchere);
             return meilleureEnchere;
         }
     }
