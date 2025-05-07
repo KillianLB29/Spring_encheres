0eb3e22186c8b51ac96bd96a23db2f09812286d8
Erwan LE MEUR <erwan.lemeur2025@campus-eni.fr>
2025-05-07 16:22:22 +0200

    ALED Kilian

--- src/main/java/fr/eni/tp/spring_encheres/ihm/AdminController.java
+++ src/main/java/fr/eni/tp/spring_encheres/ihm/AdminController.java
@@ -2,6 +2,7 @@ package fr.eni.tp.spring_encheres.ihm;
 
 import fr.eni.tp.spring_encheres.bo.Utilisateur;
 import fr.eni.tp.spring_encheres.ihm.dto.UtilisateurDTO;
+import jakarta.servlet.http.HttpSession;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -13,20 +14,24 @@ public class AdminController {
     @GetMapping("/admin")
     public String afficherPageAdmin(@ModelAttribute("utilisateurSession") Utilisateur utilisateurSession, Model model) {
 
-        // Si l'utilisateur n'est pas connecté
-        if (utilisateurSession == null || utilisateurSession.getPseudo() == null) {
-            return "redirect:/login";
+        // Si l'utilisateur n'est pas connecté ou n'est pas un administrateur
+        if (utilisateurSession == null || utilisateurSession.getPseudo() == null || !utilisateurSession.isAdmin()) {
+            return "redirect:/login";  // Rediriger vers la page de login
         }
 
-        // Si l'utilisateur n'est pas admin
-        if (!utilisateurSession.isAdmin()) {
-            return "redirect:/";
-        }
-
-        // Ajouter le DTO à la vue
+        // Si l'utilisateur est admin, on prépare l'objet UtilisateurDTO
         UtilisateurDTO utilisateurDTO = new UtilisateurDTO(utilisateurSession);
         model.addAttribute("utilisateurDTO", utilisateurDTO);
 
-        return "admin"; // renvoie vers admin.html
+        return "admin";
+    }
+
+    // Modèle qui récupère l'utilisateur de la session
+    @ModelAttribute("utilisateurSession")
+    public Utilisateur utilisateurSession(HttpSession session) {
+        Utilisateur utilisateur = (Utilisateur) session.getAttribute("utilisateurSession");
+        System.out.println("utilisateur session : " + utilisateur);
+        return utilisateur != null ? utilisateur : new Utilisateur(); // Retourner un utilisateur vide si aucun utilisateur n'est connecté
     }
 }
+
